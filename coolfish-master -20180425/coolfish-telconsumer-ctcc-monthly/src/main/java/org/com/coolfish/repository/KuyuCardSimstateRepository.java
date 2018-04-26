934e46b5723fcafd44d8aa352409feabd299bfe3
LINGHUI <LINGHUI@DESKTOP-KQUPCBR>
2018-04-26 18:53:08 +0800

    流量监控+数据同步Version1.0

--- coolfish-master -20180425/coolfish-telconsumer-ctcc-monthly/src/main/java/org/com/coolfish/repository/KuyuCardSimstateRepository.java
+++
@@ -1,7 +0,0 @@
-package org.com.coolfish.repository;
-
-import org.com.coolfish.entity.KuyuCardSimstate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KuyuCardSimstateRepository extends JpaRepository<KuyuCardSimstate, Integer> {
-}
